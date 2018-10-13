b7cfc42e2c3c13379c3fcaf5eb925c6d12823760
redaxmedia <info@redaxmedia.com>
2018-10-13 14:07:18 +0200

    Zero downtown deployment and split into parts

--- tasks/ftpress.js
+++ tasks/ftpress.js
@@ -10,11 +10,19 @@ module.exports = grunt =>
 			[
 				'build'
 			],
-			dest: 'www/htdocs/w00976cb/ncss',
+			dest: 'www/htdocs/w00976cb/ncss-relaunch',
 			options:
 			{
 				url: process.env.FTP_URL,
-				command: 'mirror {SOURCE} {TARGET} --reverse --delete-first --parallel=10 --use-pget-n=10; chmod 0444 www/htdocs/w00976cb/ncss/config.php; exit'
+				command:
+				[
+					'mirror {SOURCE} {TARGET}-new-{TIMESTAMP} --reverse --delete-first --parallel=10 --use-pget-n=10',
+					'chmod 0444 {TARGET}-new-{TIMESTAMP}/config.php',
+					'mv {TARGET} {TARGET}-old-{TIMESTAMP}',
+					'mv {TARGET}-new-{TIMESTAMP} {TARGET}',
+					'rm -rf {TARGET}-old-{TIMESTAMP}',
+					'exit'
+				]
 			}
 		}
 	};
--- tasks/shell.js
+++ tasks/shell.js
@@ -10,8 +10,7 @@ module.exports = () =>
 			[
 				'cd build',
 				'php console.php config parse --db-url $DB_URL',
-				'php console.php status database',
-				'rm console.php'
+				'php console.php status database'
 			]
 			.join('&&')
 		},
@@ -22,6 +21,15 @@ module.exports = () =>
 				'replace \'UA-00000000-0\' \'UA-16122280-17\' -- build/modules/Analytics/assets/scripts/init.js',
 				'replace \'auto\' \'ncss.io\' -- build/modules/Analytics/assets/scripts/init.js',
 				'echo sitemap: https://ncss.io/module/sitemap-xml > build/robots.txt'
+			]
+			.join('&&')
+		},
+		removeConsole:
+		{
+			command:
+			[
+				'cd build',
+				'rm console.php'
 			]
 			.join('&&')
 		},
