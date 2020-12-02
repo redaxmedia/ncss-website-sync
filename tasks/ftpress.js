8318c0e9e0feb7b374f283e2cb0606d8412a0b57
redaxmedia <info@redaxmedia.com>
2020-12-02 15:18:57 +0100

    Use ftpress beta

--- tasks/ftpress.js
+++ tasks/ftpress.js
@@ -14,8 +14,6 @@ module.exports = () =>
 				url: process.env.FTP_URL,
 				command:
 				[
-					'debug 1',
-					'set sftp:auto-confirm yes',
 					'mirror {SOURCE} {TARGET}-new-{TIMESTAMP} --reverse --delete-first --parallel=10 --use-pget-n=10',
 					'chmod 0444 {TARGET}-new-{TIMESTAMP}/config.php',
 					'mv {TARGET} {TARGET}-old-{TIMESTAMP}',
